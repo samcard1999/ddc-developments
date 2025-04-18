8c73bbef0feb94fbd54b92435eb908c3ea8e3ae8
samcard1999 <samcard1999@gmail.com>
2025-04-18 23:58:23 +0000

    Build: (bd20fdf) first commit

--- src/utils/index.js
+++
@@ -1,5 +0,0 @@
-import logo_circle from "../assets/svg/logo_circle.svg"
-import logo_letters from "../assets/svg/logo_letters.svg"
-
-export const LogoCircle = logo_circle;
-export const LogoLetters = logo_letters;
