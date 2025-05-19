8c914c8427a0f8db6bf57e0aaf84536c104e3f71
Samuel Cardenas <samcard1999@gmail.com>
2025-05-19 04:27:24 -0500

    hhooh

--- src/components/Investments_mobile.jsx
+++ src/components/Investments_mobile.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Investments_mobile = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  useLayoutEffect(() => {
+    gsap.utils.toArray(".animated_1").forEach((element) => {
+      gsap.from(element, {
+        scrollTrigger: {
+          trigger: element, // Cada elemento será su propio trigger
+          start: "40px bottom",
+          end: "bottom+=50px bottom",
+          scrub: false,
+
+          toggleActions: "play none none none"
+        },
+        right: "300px",
+        filter: "blur(10px)",
+        opacity: 0.1,
+        duration: 1,
+        ease: "power1.inOut"
+      });
+    });
+    gsap.utils.toArray(".animated_2").forEach((element) => {
+      gsap.from(element, {
+        scrollTrigger: {
+          trigger: element, // Cada elemento será su propio trigger
+          start: "40px bottom",
+          end: "bottom+=50px bottom",
+          scrub: false,
+
+          toggleActions: "play none play none"
+        },
+        left: "300px",
+        filter: "blur(10px)",
+        opacity: 0.1,
+        duration: 1,
+        ease: "power1.inOut"
+      });
+    });
+  }, []);
+
   return (
     <section className="min-h-screen w-full h-auto flex gap-3 z-10 pt-20 pb-5 px-5 ">
       <div className="flex flex-col gap-3 h-full">
-        <div className="bento_card flex flex-col h-full w-full gap-16 justify-between relative fade-in-on-scroll">
+        <div className="animated_2 bento_card flex flex-col h-full w-full gap-16 justify-between relative">
           <h1 className="text-dark_blue text-3xl font-bold">
             Why Invest with DDC <br />
             Developments?
@@ -14,7 +55,7 @@ const Investments_mobile = () => {
             Download <strong>PDF</strong>
           </h2>
         </div>
-        <div className="bento_card flex flex-col gap-3 fade-in-on-scroll">
+        <div className="animated_1 bento_card flex flex-col gap-3 ">
           <img
             className="w-full h-auto"
             alt="ROI.svg"
@@ -23,7 +64,7 @@ const Investments_mobile = () => {
           <h1 className="font-bold text-6xl text-dark_blue">30%</h1>
           <h2 className="font-bold">Lower Costs - Maximized ROI</h2>
         </div>
-        <div className="bento_card flex flex-col gap-6 fade-in-on-scroll">
+        <div className="animated_2 bento_card flex flex-col gap-6 ">
           <h1 className="text-dark_blue text-xl text-left">
             Faster <b>Returns</b>
           </h1>
@@ -33,12 +74,12 @@ const Investments_mobile = () => {
             className="w-full h-auto object-cover"
           />
         </div>
-        <div className="bento_card fade-in-on-scroll">
+        <div className="animated_1 bento_card fade-in-on-scroll">
           <h1 className="text-dark_blue text-2xl text-center">
             Proven <b>Structural Strength</b> Durable, reliable assets
           </h1>
         </div>
-        <div className="bento_card flex flex-col gap-1 h-auto relative fade-in-on-scroll">
+        <div className="animated_2 bento_card flex flex-col gap-1 h-auto relative ">
           <h1 className="text-dark_blue text-3xl font-bold text-left leading-10 tracking-[0.2rem]">
             Smart, Scalable Entry <br />
             Points
